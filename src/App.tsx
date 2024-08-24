591658660f99b97799859a973ef8a45ac1d4d56d
Ayan Sarma <ayansarmajrht@gmail.com>
2024-08-24 20:38:08 +0530

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card from './components/Card/Card'
+import Header from './components/Header/Header'
+function App() {
+
+    return (
+        <>
+            <Header />
+            <div className='card-container'>
+                <Card />
+            </div>
+        </>
+    )
+}
+
+export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+function Header() {
+  return (
+    <a href='/'>
+        <img src="src/assets/logo.png" alt="logo"  className='logo-container'/>
+    </a>
+  )
+}
+
+export default Header
